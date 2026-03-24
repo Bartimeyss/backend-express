a5177fc632b2368592e0a14e4acbe04a1cd767bb
Nezeri <ichigvintsev@icloud.com>
2026-03-24 17:07:02 +0500

    1-3

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -2,8 +2,22 @@ const express = require('express');
 const router = express.Router();
 
 /* GET users listing. */
+
+const usrs = {
+  items: []
+};
+
+usrs.items.push({
+  "id": 1,
+  "name": "Чигвинцев Иван"
+});
+usrs.items.push({
+  "id": 2,
+  "name": "Ипатов Игорь"
+});
+
 router.get('/', function(req, res, next) {
-  res.send('Hello, world!');
+  res.send(usrs);
 });
 
 module.exports = router;
