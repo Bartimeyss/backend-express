5c6578b9a973363d1f0f24ec6f824dbfa41c693f
Nezeri <ichigvintsev@icloud.com>
2026-03-24 17:12:07 +0500

    4

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -11,10 +11,13 @@ usrs.items.push({
   "id": 1,
   "name": "Чигвинцев Иван"
 });
+
+
 usrs.items.push({
   "id": 2,
   "name": "Ипатов Игорь"
 });
+
 
 router.get('/', function(req, res, next) {
   res.send(usrs);
